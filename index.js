be41a127cce6ef2756f1ff6a07293c0a0bb07751
KonstantinTheRed <61480260+KonstantinTheRed@users.noreply.github.com>
2020-08-02 06:46:08 -0700

    tutorial

--- index.js
+++ index.js
@@ -1,46 +1,33 @@
-import GlobalState from "../../../src/GlobalState";
+import React, { Component } from "react";
+try {
+  var GlobalState = require("../../../src/G_State.js");
+} catch (ex) {
+  console.error(
+    "Please add a GlobalState.js file containing an object with all the global properties"
+  );
+}
+var now = GlobalState.default;
 
-var currentAction = "";
-var now = GlobalState;
-
-const show = () => {
-  return now;
-};
-
-const mergeState = (component, now) => {
-  if (!Object.keys(component.state).length) return component.state;
-  Object.entries(now).forEach((property) => {
-    if (component.state.hasOwnProperty(property[0])) {
-      component.state[property[0]] = property[1];
-    }
-  });
-  return component.state;
-};
-
-const updateState = (component, changes) => {
-  let isChangedComponent = false;
-  changes.changedProperties.forEach((property) => {
-    if (component.state[property] !== undefined) {
-      isChangedComponent = true;
-    }
-  });
-  if (isChangedComponent) {
-    component.setState(mergeState(component, now));
+const debug = (args) => {
+  if (args) {
+    Object.keys(args).forEach((arg) => {
+      if (debugArgList[arg] !== undefined) debugArgList[arg] = arg;
+      else console.log("not an valid setting");
+    });
   }
+  debugArgList.isDebugging = true;
+  window.G_events = events;
 };
-
-const updates = (component) => {
-  document.addEventListener("stateChanged", (event) => {
-    updateState(component, event.detail);
-  });
-  component.state = mergeState(component, now);
-};
-
 const changesTo = (changes) => {
   let changedProperties = [];
   Object.entries(changes).forEach((change) => {
-    currentAction = change[1]();
     changedProperties.push(change[0]);
+    currentEvent = change[0];
+
+    if (debugArgList.isDebugging) {
+      createEvent(change);
+      debug();
+    } else change[1]();
   });
   document.dispatchEvent(
     new CustomEvent("stateChanged", {
@@ -52,6 +39,156 @@ const changesTo = (changes) => {
   );
 };
 
+Object.entries(now).forEach((property) => {
+  if (typeof property[1] === "object" && property[1].hasOwnProperty("value"))
+    now[property[0]].changesTo = (event, value) => {
+      let newState = {};
+      newState[property[0]] = {};
+      newState[property[0]] = () => {
+        now[property[0]].value = value;
+        return event + "_" + property[0];
+      };
+      changesTo(newState);
+    };
+});
+
+var events = [];
+
+var debugArgList = { isDebugging: false, showInitialMount: false, live: false };
+
+let currentEvent = "";
+
+const show = () => {
+  if (now) return now;
+  else return "There are no properties in the global state.";
+};
+
+const createEvent = (change) => {
+  if (debugArgList.isDebugging) {
+    let initialType = typeof State.now[change[0]];
+    let event = {
+      event: change[1](),
+    };
+    event[
+      typeof change[0] !== "string"
+        ? typeof change[0] === "object" && change[0].G_ID
+          ? "component"
+          : change[0]
+        : change[0]
+    ] =
+      typeof change[0] === "string" && !change[0].G_ID
+        ? State.now[change[0]]
+        : typeof change[0] == "object" && change[0].G_ID
+        ? change[0]._reactInternalFiber._debugSource.fileName +
+          ":" +
+          change[0]._reactInternalFiber._debugSource.lineNumber
+        : State.now[change[0]]
+        ? State.now[change[0]]
+        : "";
+    //Why you should not write logic this
+    events.push(event);
+    if (debugArgList.live)
+      console.log(events.length, events[events.length - 1]);
+  }
+};
+
+const updateState = (componentLink, changes) => {
+  let isChangedComponent = false;
+  changes.changedProperties.forEach((property) => {
+    if (componentLink.G_dependancies.hasOwnProperty(property)) {
+      isChangedComponent = true;
+    }
+  });
+
+  if (isChangedComponent) {
+    createEvent([
+      componentLink.component,
+      () => {
+        return "rendering " + componentLink.component.G_ID;
+      },
+    ]);
+    componentLink.component.forceUpdate();
+  }
+};
+
+const commit = (actions) => {
+  if (actions) {
+    Object.entries(actions).forEach((action) => {
+      let newState = {};
+      newState[action[0]] = action[1];
+      let event = Object.keys(newState)[0];
+      if (currentEvent !== event && currentEvent !== "initial_mount") {
+        currentEvent = Object.keys(newState)[0];
+
+        changesTo(newState);
+      }
+    });
+  }
+};
+
+const updates = (component, state, actions) => {
+  if (debugArgList.showInitialMount) {
+    currentEvent = "initial_mount";
+    let initialEvent = [
+      "initial_mount",
+      () => {
+        return "initial_mount";
+      },
+    ];
+
+    createEvent(initialEvent);
+  }
+  if (typeof component === "function") {
+    return instantiate(component, state, actions);
+  } else if (typeof component === "object") {
+    if (!component.G_ID) component.G_ID = component.constructor.name;
+
+    if (actions) {
+      component.componentDidUpdate = () => {
+        commit(actions);
+      };
+    }
+
+    let componentLink = {
+      component: component,
+      G_dependancies: component.state.G_dependancies,
+    };
+
+    let heardUpdate = (event) => {
+      return updateState(componentLink, event.detail);
+    };
+
+    document.addEventListener("stateChanged", heardUpdate);
+    component.componentWillUnmount = () => {
+      document.removeEventListener("stateChanged", heardUpdate);
+    };
+  }
+};
+
+const instantiate = (ChildComponent, G_dependancies, actions) => {
+  class Comp extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {};
+      this.state.G_dependancies = G_dependancies;
+      this.G_ID = ChildComponent.name;
+      updates(this);
+    }
+
+    componentDidUpdate() {
+      commit(actions);
+    }
+
+    render() {
+      return React.createElement(
+        React.Fragment,
+        null,
+        React.createElement(ChildComponent)
+      );
+    }
+  }
+  return Comp;
+};
 const addProperty = (propertyName, property) => {
   now[propertyName] = property;
 };
@@ -60,14 +197,23 @@ const removeProperty = (propertyName) => {
   delete now[propertyName];
 };
 
+const link = (...args) => {
+  let dependancies = {};
+  args.forEach((dependancy) => {
+    dependancies[dependancy] = null;
+  });
+  return dependancies;
+};
+
 var State = {
   now,
+  link,
   show,
-  updateState,
   updates,
   addProperty,
   removeProperty,
   changesTo,
+  debug,
 };
 
 export default State;
